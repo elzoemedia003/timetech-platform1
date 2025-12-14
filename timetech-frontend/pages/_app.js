2a3648fbcebf17dc0963bbf72580eef6feb62fc3
elzoemedia003 <elzoemedia003@gmail.com>
2025-12-14 16:01:19 +0100

    Update _app.js

--- timetech-frontend/pages/_app.js
+++ timetech-frontend/pages/_app.js
@@ -1,11 +1,7 @@
-import "../styles/globals.css";
 import Layout from "../components/Layout";
+import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }) {
-  // Pages can opt out of Layout by setting Component.noLayout = true
-  if (Component.noLayout) {
-    return <Component {...pageProps} />;
-  }
   return (
     <Layout>
       <Component {...pageProps} />
